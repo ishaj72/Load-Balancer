daf5d7fd99a824bb1a69fee0fb69c25dc1ef7b17
Isha <isha30435@gmail.com>
2026-08-19 21:34:03 +0530

    Remove untracked build, IDE, and configuration files

--- loadBalancer/src/main/java/com/example/loadBalancer/config/DynamicFeignConfiguration.java
+++ loadBalancer/src/main/java/com/example/loadBalancer/config/DynamicFeignConfiguration.java
@@ -13,8 +13,7 @@ public class DynamicFeignConfiguration {
     @Bean
     public RequestInterceptor loadBalancerInterceptor(ServerRegistry serverRegistry,LoadBalancingStrategy loadBalancingStrategy){
         return requestTemplate -> {
-            Server server = loadBalancingStrategy.selectServer(
-                    serverRegistry.getServers());
+            Server server = loadBalancingStrategy.selectServer(serverRegistry.getServers());
 
             String url = "http://" +
                     server.getHost() +
